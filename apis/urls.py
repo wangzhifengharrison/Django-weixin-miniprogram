1bfde40f874c7f443fd6027200afb3b207991efb
zhifeng wang <1124236437@qq.com>
2019-06-21 17:02:04 +1000

    3-9,3-10

--- apis/urls.py
+++ apis/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
 
-from apis.views import menu, weather, image
+from apis.views import menu, weather, image, service
 
 # from .views import weather
 urlpatterns =[
@@ -9,11 +9,19 @@ urlpatterns =[
     # path('',weather.helloworld)
     # path('',weather.weather)
     #path('weather',weather.weather),
-    path('weather',weather.WeatherView.as_view()),
-    path('menu', menu.get_menu),
+    #path('weather',weather.WeatherView.as_view()),
+    #path('menu', menu.get_menu),
     # path('image', image.image),
     # http://127.0.0.1:8000/api/v1.0/service/imagetext?md5=02bcecdf1bcbe1a2c446097ea657618c
     # path('imagetext', image.image_text)
+    # path('image', image.ImageView.as_view()),
+    # path('image/list', image.ImageListView.as_view()),
+
+    path('weather', weather.WeatherView.as_view()),
+    path('menu', menu.get_menu),
     path('image', image.ImageView.as_view()),
-    path('image/list', image.ImageListView.as_view())
+    path('image/list', image.ImageListView.as_view()),
+    path('stock', service.stock),
+    path('constellation', service.constellation),
+    path('joke', service.joke)
 ]
--- thirdparty/juhe.py
+++ thirdparty/juhe.py
@@ -1,5 +1,8 @@
 import json
 import requests
+import time
+
+from utils import proxy
 
 
 def weather(cityname):
@@ -18,13 +21,84 @@ def weather(cityname):
     print(json_data)
 
     response = dict()
-    response['temperature']=json_data.get('main').get('temp')
-    response['wind_direction'] =json_data.get('wind').get('deg')
+    response['temperature'] = json_data.get('main').get('temp')
+    response['wind_direction'] = json_data.get('wind').get('deg')
     response['wind_strength'] = json_data.get('wind').get('speed')
     response['humidity'] = json_data.get('main').get('humidity')
     response['time'] = json_data.get('dt')
     print(response)
     return response
+
+
+# third party information
+def constellation(cons_name):
+    '''
+    :param cons_name: 星座名字
+    :return: json 今天运势
+    '''
+    key = '638590d043a54639f3560b5381f5c4f0'
+    api = 'http://web.juhe.cn:8080/constellation/getAll'
+    types = ('today', 'tomorrow', 'week', 'month', 'year')
+    params = 'consName=%s&type=%s&key=%s' % (cons_name, types[0], key)
+    url = api + '?' + params
+    print(url)
+    response = requests.get(url=url, proxies=proxy.proxy())
+    data = json.loads(response.text)
+    return {
+        'name': cons_name,
+        'text': data['summary']
+    }
+
+
+# third party information
+def stock(market, code):
+    '''
+    沪深股票
+    :param market: 上交所 = sh, 深交所 = sz
+    :param code: 股票编号
+    :return:
+    '''
+    key = 'f887b09847c9bcde9801ca7aaa86513e'
+    api = 'http://web.juhe.cn:8080/finance/stock/hs'
+    params = 'gid=%s&key=%s' % (market + code, key)
+    url = api + '?' + params
+    print(url)
+    response = requests.get(url=url, proxies=proxy.proxy())
+    data = json.loads(response.text)
+    data = data.get('result')[0].get('data')
+    response = {
+        'name': data.get('name'),
+        'now_price': data.get('nowPri'),
+        'today_min': data.get('todayMin'),
+        'today_max': data.get('todayMax'),
+        'start_price': data.get('todayStartPri'),
+        'date': data.get('date'),
+        'time': data.get('time')
+    }
+    response['is_rising'] = data.get('nowPri') > data.get('todayStartPri')
+    sub = abs(float(data.get('nowPri')) - float(data.get('todayStartPri')))  # 差值
+    response['sub'] = float('%.3f' % sub)
+    return response
+
+
+# third party information
+def history_today():
+    key = '6c6b318d983b6b4ac8cc5cda0da92155'
+    api = 'http://api.juheapi.com/japi/toh'
+    month = time.localtime().tm_mon
+    day = time.localtime().tm_mday
+    params = 'v=1.0&month=%d&day=%d&key=%s' % (month, day, key)
+    url = api + '?' + params
+    response = requests.get(url=url, proxies=proxy.proxy())
+    data = json.loads(response.text)
+    result_list = data.get('result')
+    result = []
+    for item in result_list:
+        result.append({
+            'title': item.get('title'),
+            'content': item.get('des')
+        })
+    return result
 
 
 if __name__ == '__main__':
