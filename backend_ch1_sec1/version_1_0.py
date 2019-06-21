4f82fcbd1ccf5e67aad704f45f0a793bac3f1203
zhifeng wang <1124236437@qq.com>
2019-06-21 17:23:13 +1000

    4-3

--- backend_ch1_sec1/version_1_0.py
+++ backend_ch1_sec1/version_1_0.py
@@ -2,5 +2,6 @@
 from django.urls import path, include
 urlpatterns =[
 
-    path('service/', include('apis.urls'))
+    path('service/', include('apis.urls')),
+    path('auth/', include('authorization.urls'))
 ]
